d7a19dd5f7abe3684b95188706fcfef6f12a31da
rajlakshmi_26 <rhimatsinghka@gmail.com>
2020-09-09 00:34:06 +0530

    Day 3

--- src/dev/rajlakshmi/dailybyte/Main.java
+++ src/dev/rajlakshmi/dailybyte/Main.java
@@ -1,6 +1,7 @@
 package dev.rajlakshmi.dailybyte;
 
 import dev.rajlakshmi.dailybyte.day1_day30.ReverseString_Day1;
+import dev.rajlakshmi.dailybyte.day1_day30.VacuumCleanerRoute_Day3;
 import dev.rajlakshmi.dailybyte.day1_day30.ValidPalindrome_Day2;
 
 public class Main {
@@ -15,6 +16,12 @@ public class Main {
         System.out.println(day2.isPalindrome("A man, a plan, a canal: Panama"));
         System.out.println(day2.isPalindrome("0P"));
         System.out.println(day2.isPalindrome("Civic"));
+
+        VacuumCleanerRoute_Day3 day3 = new VacuumCleanerRoute_Day3();
+        System.out.println(day3.isRouteValid("LR"));
+        System.out.println(day3.isRouteValid("URURD"));
+        System.out.println(day3.isRouteValid("RUULLDRD"));
+
         // write your code here
     }
 }
---
+++ src/dev/rajlakshmi/dailybyte/day1_day30/VacuumCleanerRoute_Day3.java
@@ -0,0 +1,26 @@
+/*
+This question is asked by Amazon. Given a string representing the sequence of moves a robot vacuum makes, return whether or not it will return to its original position. The string will only contain L, R, U, and D characters, representing left, right, up, and down respectively.
+
+Ex: Given the following strings...
+
+"LR", return true
+"URURD", return false
+"RUULLDRD", return true
+ */
+package dev.rajlakshmi.dailybyte.day1_day30;
+public class VacuumCleanerRoute_Day3 {
+    public boolean isRouteValid(String route) {
+        int n = route.length(), x = 0, y = 0;
+        for(int i=0; i<n; i++) {
+            if(route.charAt(i) == 'U')
+                y--;
+            else if(route.charAt(i) == 'D')
+                y++;
+            else if(route.charAt(i) == 'L')
+                x--;
+            else if(route.charAt(i) == 'R')
+                x++;
+        }
+        return (x == 0 && y == 0);
+    }
+}
